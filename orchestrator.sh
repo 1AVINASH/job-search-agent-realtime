fafe2069a0aca7121f353b0c2d994f1740158873
avinash <avinash24061998@gmail.com>
2025-08-07 15:57:15 +0530

    Make current directory as cwd in the orchestrator so it can be run through cron

--- orchestrator.sh
+++ orchestrator.sh
@@ -2,6 +2,8 @@
 set -e  # exit on error
 set -o pipefail
 
+cd "$(dirname "$0")"
+
 echo "Starting job scraper"
 docker compose up job-scraper --abort-on-container-exit &
 docker compose up linkedin-job-scraper --abort-on-container-exit &
